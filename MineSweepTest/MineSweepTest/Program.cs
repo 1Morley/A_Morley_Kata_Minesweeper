c6d3043b15f6ffb04d01695618d3e3cfdb15e422
1Morley <amorley@student.neumont.edu>
2025-10-13 11:54:51 -0600

    Add Files

---
+++ MineSweepTest/MineSweepTest/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MineSweepTest;
+
+Console.WriteLine("Hello, World!");
+MineSweepGame game = new MineSweepGame();
+bool firstTurn = true;
+bool cont = true;
+while (cont) {
+    int inputX;
+    int inputY;
+    Console.WriteLine("input column");
+    int.TryParse(Console.ReadLine(), out inputY);
+
+    Console.WriteLine("input row");
+    int.TryParse(Console.ReadLine(), out inputX);
+
+
+
+    if (firstTurn)
+    {
+        game.CreateBoard(inputX, inputY);
+        firstTurn = false;
+    }
+    else {
+        game.markItem(inputX, inputY);
+    }
+
+    for (int x = 0; x < game.Board.GetLength(0); x++) {
+        for (int y = 0; y < game.Board.GetLength(1); y++) {
+            Console.Write(game.Board[x,y].BombLabel + " ");
+        }
+        Console.WriteLine();
+    }
+
+    if (!cont) {
+        Console.WriteLine("\n\nBOMB HIT");
+    }
+    
+}
